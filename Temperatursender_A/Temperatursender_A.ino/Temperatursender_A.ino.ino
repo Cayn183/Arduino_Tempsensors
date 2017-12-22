093c99045e06d37b466ccc3d6f9e35317eb1ea75
Cayn183 <cayn111@web.de>
2017-12-22 15:03:56 +0100

    Sendercode erweitert
    Spannungsmessung eingefügt

---
+++ Temperatursender_A/Temperatursender_A.ino/Temperatursender_A.ino.ino
@@ -0,0 +1,75 @@
+#include <RCSwitch.h> //RCSwitch-Libary einfügen
+#include <DHT.h>  //DHT-Sensor Libary einfügen
+
+
+#define DHTPIN 7  //Definiere Sensor an PIN7
+#define DHTTYPE DHT22 //Definiere DHT22 als Sensor
+DHT dht(DHTPIN,DHTTYPE);  
+RCSwitch mySwitch = RCSwitch();
+
+
+int voltinput = 0; //Definiere Variable Voltinput als 0
+float vout = 0.0; //Definiere Variable Vout als 0
+float vin = 0.0;  //Definiere Variable Vin als 0
+
+float R1 = 100200.0;  //Definiere Wiederstand R1
+float R2 = 9960.0;  //Definiere Wiederstand R2
+int value = 0;  //Definiere Value Pin als A0
+int Data = 7;   //Definiere Data Pin als D7
+
+
+void setup()
+{
+  Serial.begin(9600); //Sarte Serielle Verbindung
+  pinMode(voltinput, INPUT);  //Definiere voltinput als Eingang
+
+  pinMode(Data, INPUT); //Definiere Data-Pin als Input
+  dht.begin();  //Starte Sensor-Messungen
+
+}
+
+void loop()
+{
+  Spannungsmessung();
+}
+
+
+
+void Spannungsmessung()
+{
+  //read tje value at analog input
+  value = analogRead(voltinput);
+  vout = (value * 4.9) / 1024.0; 
+  vin = vout / (R2/(R1+R2));
+  
+  if (vin<0.09)
+  {
+     vin=0.0;
+  }
+
+  Serial.print("Spannung: ");
+  Serial.print(vin, 1);
+  Serial.println("V");
+  delay(5000);
+
+  Temperaturmessung();
+}
+
+
+void Temperaturmessung()
+{
+  float Luftfeuchtigkeit = dht.readHumidity();
+  float Temperatur = dht.readTemperature();
+
+  
+  
+  Serial.println(Luftfeuchtigkeit, 2);
+  Serial.println(Temperatur, 2);
+
+
+    
+  mySwitch.send(Temperatur, 2);
+  mySwitch.send(Luftfeuchtigkeit, 2);
+  delay(10000);
+}
+
