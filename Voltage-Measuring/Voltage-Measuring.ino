6e249c538acdbde6a1432901a9a29ef34fa0095a
Cayn183 <cayn111@web.de>
2017-12-22 11:55:39 +0100

    Korrigierte Messwerte

--- Voltage-Measuring/Voltage-Measuring.ino
+++ Voltage-Measuring/Voltage-Measuring.ino
@@ -4,8 +4,8 @@ int analogInput = 0;
 float vout = 0.0;
 float vin = 0.0;
 
-float R1 = 100000.0;
-float R2 = 10000.0;
+float R1 = 100200.0;
+float R2 = 9960.0;
 int value = 0;
 
 
@@ -21,7 +21,7 @@ void loop()
 {
   //read tje value at analog input
   value = analogRead(analogInput);
-  vout = (value * 5.0) / 1024.0; 
+  vout = (value * 4.9) / 1024.0; 
   vin = vout / (R2/(R1+R2));
   
   if (vin<0.09)
@@ -30,6 +30,6 @@ void loop()
   }
 
   Serial.print("Spannung: ");
-  Serial.println(vin, 3);
+  Serial.println(vin, 1);
   delay(5000);
 }
